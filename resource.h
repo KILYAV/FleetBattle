10c66cc31728c640196ed99da381812551aa1460
KILYAV <K_ILYA_V@INBOX.RU>
2024-10-03 15:59:21 +0300

    UpDate resource

--- resource.h
+++ resource.h
@@ -14,8 +14,11 @@
 #define IDI_SMALL                       108
 #define IDC_FLEETBATTLE                 109
 #define IDR_MAINFRAME                   128
-#define IDR_MENU1                       129
 #define ID_BATTLE_START                 32776
+#define ID_RANDOM                       32777
+#define ID_RANDOM_RANDOM                32778
+#define ID_BATTLE_BATLLE                32779
+#define ID_BATTLE_BATTLE                32780
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -24,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
